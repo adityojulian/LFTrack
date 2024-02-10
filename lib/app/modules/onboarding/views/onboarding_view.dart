f7c75dd6459af8cb38c7267d5857e94f70cf600c
adityojulian01 <adityojulian.twm@gmail.com>
2024-02-10 02:42:01 +0000

    implemented on-boarding controller and widget

--- lib/app/modules/onboarding/views/onboarding_view.dart
+++ lib/app/modules/onboarding/views/onboarding_view.dart
@@ -13,107 +13,89 @@ class OnboardingView extends GetView<OnboardingController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(40),
-          child: AppBar(
-            actions: [
-              TextButton(
-                onPressed: () {
-                  controller.indicator.jumpToPage(3);
-                },
-                child: const Text('Skip'),
-              ),
-            ],
+        body: Stack(
+      children: [
+        PageView(
+          controller: controller.indicator,
+          onPageChanged: ((value) {
+            controller.page.value = value;
+            // print(controller.page.value);
+          }),
+          children: const [
+            // start page onboarding
+            OnBoardingWidgets(
+              image: 'on_boarding_1.png',
+              title: 'Get LFT Ready',
+              subtitle:
+                  'Place Lateral Flow Test (LFT) on top of a flat surface',
+            ),
+            OnBoardingWidgets(
+              image: 'on_boarding_2.png',
+              title: 'Adjust Phone\'s Position',
+              subtitle:
+                  'Frame the LFT to be in the middle of the camera preview by adjusting phone\'s position and angle',
+            ),
+            OnBoardingWidgets(
+              image: 'on_boarding_3.png',
+              title: 'Let AI do the works!',
+              subtitle:
+                  'Wait for countdown to finish and the result will be automatically stored',
+            ),
+            // end
+          ],
+        ),
+        Container(
+          alignment: const Alignment(0.8, -0.85),
+          child: GestureDetector(
+            onTap: () {
+              controller.indicator.jumpToPage(3);
+            },
+            child: Text(
+              'Skip',
+              style: semibold.copyWith(
+                  fontSize: 16, color: Theme.of(context).colorScheme.primary),
+            ),
           ),
         ),
-        body: Column(
-          children: [
-            Container(
-              height: Get.height * 0.7,
-              child: PageView(
-                controller: controller.indicator,
-                onPageChanged: ((value) {
-                  controller.page.value = value;
-                  // print(controller.page.value);
-                }),
-                children: const [
-                  // start page onboarding
-                  OnBoardingWidgets(
-                    image: 'on_boarding_1.png',
-                    title: 'Track Everything',
-                    subtitle:
-                        'your financial journey start here. We are\nhere to help ypur tracking and handle every\ntransaction 24/day.',
-                  ),
-                  OnBoardingWidgets(
-                    image: 'on_boarding_2.png',
-                    title: 'Fast Transaction',
-                    subtitle:
-                        'Get easy to pay all yours bills with just a\nfew taps. paying your bill become fast \nand efficient. Enjoy',
-                  ),
-                  OnBoardingWidgets(
-                    image: 'on_boarding_3.png',
-                    title: 'Enjoy Everyday!',
-                    subtitle:
-                        'Be wise, and discover your best financial\nexperience with us. Everything is in\nyour hands!',
-                  ),
-                  // end
-                ],
-              ),
-            ),
-            // const Expanded(
-            //   child: SizedBox.shrink(),
-            // ),
-            // Container(
-            //   alignment: Alignment(0.8, -0.85),
-            //   child: GestureDetector(
-            //     onTap: () {
-            //       controller.indicator.jumpToPage(3);
-            //     },
-            //     child: Text('Skip'),
-            //   ),
-            // ),
-            Obx(() => Container(
-                  // alignment: Alignment(0, 0.85),
-                  child: controller.page.value != 2
-                      ? SmoothPageIndicator(
-                          controller: controller.indicator,
-                          count: 3,
-                          effect: ScrollingDotsEffect(
-                            // fixedCenter: true,
-                            activeDotColor:
-                                Theme.of(context).colorScheme.primary,
-                            spacing: 10,
-                            radius: 6,
-                            dotWidth: 10,
-                            dotHeight: 10,
-                            dotColor:
-                                Theme.of(context).colorScheme.inversePrimary,
-                          ),
-                        )
-                      : GestureDetector(
-                          onTap: () {
-                            Get.offAllNamed(Routes.HOME);
-                          },
-                          child: Container(
-                            height: 55,
-                            width: Get.width * 0.8,
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.symmetric(vertical: 14),
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).colorScheme.primary,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Text(
-                              'Getting Started',
-                              style: semibold.copyWith(
-                                fontSize: 18,
-                                color: Colors.white,
-                              ),
-                            ),
+        Obx(() => Container(
+              alignment: Alignment(0, controller.page.value == 2 ? 0.88 : 0.85),
+              child: controller.page.value != 2
+                  ? SmoothPageIndicator(
+                      controller: controller.indicator,
+                      count: 3,
+                      effect: const ScrollingDotsEffect(
+                        activeDotColor: purple,
+                        spacing: 8.0,
+                        radius: 4.0,
+                        dotWidth: 8,
+                        dotHeight: 8,
+                        dotColor: darkGrey,
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        Get.offAllNamed(Routes.HOME);
+                      },
+                      child: Container(
+                        height: 55,
+                        width: Get.width * 0.8,
+                        alignment: Alignment.center,
+                        // padding: EdgeInsets.symmetric(vertical: 14),
+                        decoration: BoxDecoration(
+                          color: purple,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          'Getting Started',
+                          style: semibold.copyWith(
+                            fontSize: 18,
+                            color: Colors.white,
                           ),
                         ),
-                ))
-          ],
-        ));
+                      ),
+                    ),
+            ))
+      ],
+    ));
   }
 }
--- lib/app/widgets/OnBoardingWidgets.dart
+++ lib/app/widgets/OnBoardingWidgets.dart
@@ -6,19 +6,53 @@ class OnBoardingWidgets extends StatelessWidget {
   final String? image;
   final String? title;
   final String? subtitle;
-  const OnBoardingWidgets({this.image, this.title, this.subtitle});
+  const OnBoardingWidgets({super.key, this.image, this.title, this.subtitle});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
+    return Container(
+      height: Get.height,
+      child: Column(
         children: [
           Container(
-            // color: Theme.of(context).colorScheme.surface,
-            color: Theme.of(context).colorScheme.secondaryContainer,
-            height: (Get.height - AppBar().preferredSize.height) * 0.4,
-            width: Get.width * 0.7,
-            // height: 400,
+            height: Get.height * 0.65,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/${image}'),
+                scale: 1.75,
+              ),
+            ),
+          ),
+          Container(
+            height: Get.height * 0.35,
+            width: Get.width,
+            decoration: BoxDecoration(
+              // borderRadius: BorderRadius.circular(25),
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+            padding: const EdgeInsets.fromLTRB(30, 50, 30, 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  '${title}',
+                  style: bold.copyWith(
+                      fontSize: 22,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  '${subtitle}',
+                  style: regular.copyWith(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
           )
         ],
       ),
